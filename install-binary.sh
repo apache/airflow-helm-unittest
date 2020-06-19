56cdcf9abdbffcea0c5aa16bf02e3efcdf255e46
Aneesh Joseph <admin@coderplus.com>
2020-06-19 16:11:41 +0530

    use sha1sum

--- install-binary.sh
+++ install-binary.sh
@@ -117,9 +117,9 @@ installFile() {
   if [ -n "$PROJECT_CHECKSUM" ]; then
     echo Validating Checksum.
     if type "curl" >/dev/null 2>&1; then
-      curl -s -L $PROJECT_CHECKSUM | grep $DOWNLOAD_FILE | shasum -a 256 -c -s
+      curl -s -L $PROJECT_CHECKSUM | grep $DOWNLOAD_FILE | sha1sum -a 256 -c -s
     elif type "wget" >/dev/null 2>&1; then
-      wget -q -O - $PROJECT_CHECKSUM | grep $DOWNLOAD_FILE | shasum -a 256 -c -s
+      wget -q -O - $PROJECT_CHECKSUM | grep $DOWNLOAD_FILE | sha1sum -a 256 -c -s
     fi
   else
     echo No Checksum validated.
